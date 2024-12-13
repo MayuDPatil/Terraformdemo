3ed0abed9dc04cc7169c5e715c6c25d8e626184a
MayuDPatil <98633609+MayuDPatil@users.noreply.github.com>
2024-12-13 08:53:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,36 @@
 resources "aws_vpc" "myvpc"{
-cidr_block = "
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+resource "aws_subnet" "sub2" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw"{
+  vpc_id = aws_vpc.myvpc.id
+}
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+}
+resource "aws_route_table_association"  "rta1"{
+  subnet_id = aws_subnet.sub1.id
+  route_table_id = aws_route_table_RT.id
+}
+resource "aws_route_table_association"  "rta1"{
+  subnet_id = aws_subnet.sub2.id
+  route_table_id = aws_route_table_RT.id
+}
+
+}
