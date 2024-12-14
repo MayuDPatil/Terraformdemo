9fc8be6f0a281a458ec7a9606c795660167fe059
MayuDPatil <98633609+MayuDPatil@users.noreply.github.com>
2024-12-14 12:52:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resources "aws_vpc" "myvpc"{
+resource "aws_vpc" "myvpc"{
   cidr_block = var.cidr
 }
 
@@ -10,7 +10,7 @@ resource "aws_subnet" "sub1" {
 }
 resource "aws_subnet" "sub2" {
   vpc_id = aws_vpc.myvpc.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.0.0/16"
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
 }
@@ -26,9 +26,74 @@ resource "aws_route_table" "RT" {
 }
 resource "aws_route_table_association"  "rta1"{
   subnet_id = aws_subnet.sub1.id
-  route_table_id = aws_route_table_RT.id
+  route_table_id = aws_route_table.RT.id
 }
 resource "aws_route_table_association"  "rta2"{
   subnet_id = aws_subnet.sub2.id
-  route_table_id = aws_route_table_RT.id
+  route_table_id = aws_route_table.RT.id
+}
+resource "aws_security_group" "allow_all" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  type              = "ingress"
+  security_group_id = aws_security_group.allow_http.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_SSL" {
+  type              = "ingress"
+  security_group_id = aws_security_group.allow_http.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  type              = "egress"
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv4         = ["0.0.0.0/0"]
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-terraform-bucket123"
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
 }
